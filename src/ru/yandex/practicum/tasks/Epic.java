56dce71879a605ea4125f68df1c00319b87096dd
Bervy <vladshelkovo@yandex.ru>
2022-04-26 23:42:07 +0300

    feat  - Добавил геттеры и сеттеры для имени и описания

--- src/ru/yandex/practicum/tasks/Epic.java
+++ src/ru/yandex/practicum/tasks/Epic.java
@@ -22,12 +22,28 @@ public class Epic extends Task {
         super.state = state;
     }
 
+    public String getName() {
+        return super.name;
+    }
+
+    public void setName(String name) {
+        super.name = name;
+    }
+
+    public String getDescription() {
+        return super.description;
+    }
+
+    public void setDescription(String description) {
+        super.description = description;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Epic epic = (Epic) o;
-        return epic.id == id;
+        return epic.id == id && epic.name.equals(name) && epic.description.equals(description);
     }
 
     @Override
