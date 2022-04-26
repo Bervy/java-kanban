fe618e6b0f1112f287ebc846b10615ed4ec2cf5e
Bervy <vladshelkovo@yandex.ru>
2022-04-26 23:43:12 +0300

    feat  - Добавил геттеры и сеттеры для имени и описания fix - изменил метод equals

--- src/ru/yandex/practicum/tasks/Task.java
+++ src/ru/yandex/practicum/tasks/Task.java
@@ -6,8 +6,8 @@ package ru.yandex.practicum.tasks;
  */
 public class Task {
 
-    protected final String name;
-    protected final String description;
+    protected String name;
+    protected String description;
     protected int id;
     protected States state;
 
@@ -48,6 +48,22 @@ public class Task {
         this.state = state;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Override
     public String toString() {
         return "Task{" +
@@ -63,6 +79,6 @@ public class Task {
         if (this == o) return true;
         if (!(o instanceof Task)) return false;
         Task task = (Task) o;
-        return id == task.id;
+        return task.id == id && task.name.equals(name) && task.description.equals(description);
     }
 }
