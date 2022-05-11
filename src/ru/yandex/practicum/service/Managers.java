c776415c1dcaaface7fbc1f58f4e0c09ee1fa6b8
Bervy <vladshelkovo@yandex.ru>
2022-05-11 19:48:14 +0300

    fix : Сделал Managers интерфейсом, так как он утилитарный и в нем только статически методы.

--- src/ru/yandex/practicum/service/Managers.java
+++ src/ru/yandex/practicum/service/Managers.java
@@ -6,13 +6,13 @@ import ru.yandex.practicum.history.InMemoryHistoryManager;
  * @author Vlad Osipov
  * @create 2022-05-09   15:42
  */
-public abstract class Managers {
+public interface  Managers {
 
-    public static TaskManager getDefault() {
+    static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
 
-    public static HistoryManager getDefaultHistory() {
+    static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
 }
