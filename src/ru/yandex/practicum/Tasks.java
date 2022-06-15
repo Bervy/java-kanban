5613b6f2ab715fa7e2d47d04139b54dde920b543
Bervy <vladshelkovo@yandex.ru>
2022-06-15 22:21:27 +0300

    fix : Изменил название

--- src/ru/yandex/practicum/Tasks.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.practicum;
-
-/**
- * @author Vlad Osipov
- * @create 2022-06-14   14:46
- */
-public enum Tasks {
-    TASK, EPIC, SUBTASK
-}
---
+++ src/ru/yandex/practicum/task/TaskType.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.task;
+
+/**
+ * @author Vlad Osipov
+ * @create 2022-06-14   14:46
+ */
+public enum TaskType {
+    TASK,
+    EPIC,
+    SUBTASK
+}
