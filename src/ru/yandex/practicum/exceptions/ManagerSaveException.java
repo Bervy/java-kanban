07916595891e065bbc7fa9d1761a5a31f443c65f
Bervy <vladshelkovo@yandex.ru>
2022-06-15 22:19:47 +0300

    fix : Унаследовался от RuntimeException

--- src/ru/yandex/practicum/exceptions/ManagerSaveException.java
+++ src/ru/yandex/practicum/exceptions/ManagerSaveException.java
@@ -4,7 +4,7 @@ package ru.yandex.practicum.exceptions;
  * @author Vlad Osipov
  * @create 2022-06-14   23:52
  */
-public class ManagerSaveException extends Exception {
+public class ManagerSaveException extends RuntimeException {
 
     public ManagerSaveException(String message) {
         super(message);
