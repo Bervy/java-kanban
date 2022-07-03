fbb9c4f2d301361092b09123ab04924e792947b3
Bervy <vladshelkovo@yandex.ru>
2022-07-03 03:17:02 +0300

    fix : отформатировал код

--- src/ru/yandex/practicum/exceptions/ManagerLoadException.java
+++ src/ru/yandex/practicum/exceptions/ManagerLoadException.java
@@ -4,7 +4,7 @@ package ru.yandex.practicum.exceptions;
  * @author Vlad Osipov
  * @create 2022-06-15   0:15
  */
-public class ManagerLoadException extends RuntimeException{
+public class ManagerLoadException extends RuntimeException {
 
     public ManagerLoadException(String message) {
         super(message);
