967aaec1912e580f5b31d282437bc1a49d0cc2ae
Bervy <vladshelkovo@yandex.ru>
2022-07-15 13:32:44 +0300

    add : добавил список типов коллекций задач

---
+++ src/ru/yandex/practicum/task/TaskCollectionType.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.task;
+
+/**
+ * @author Vlad Osipov
+ * @create 2022-07-15   11:23
+ */
+public enum TaskCollectionType {
+    TASKS,
+    EPICS,
+    SUBTASKS,
+    HISTORY_TASKS,
+    PRIORITIZED_TASKS
+}
